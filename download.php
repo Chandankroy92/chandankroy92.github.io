c4e7a4c9b49517b96c83efad43a38635720f0e89
Chandan <chandan@redbaton.co>
2019-07-02 15:55:58 +0530

    cv

--- download.php
+++ download.php
@@ -2,8 +2,8 @@
 header("Content-type:application/pdf");
 
 // It will be called downloaded.pdf
-header("Content-Disposition:attachment;filename='chandankumar-resume.pdf'");
+header("Content-Disposition:attachment;filename=chandankumar-resume.pdf");
 
 // The PDF source is in original.pdf
-readfile("chandan-cv.pdf");
+readfile("https://chandankroy92.github.io/chandan-cv.pdf");
 ?>
